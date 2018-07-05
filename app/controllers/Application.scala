589ff8336e130db6b436a740ee31f1f4f0ceb14e
natyur <natalia.yurkovets@gmail.com>
2018-07-05 09:32:04 +0200

    chore(project): update send-view messages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,19 +6,35 @@ import java.io._
 import scala.io.Source
 
 class Application extends Controller {
-
+  var fileCounter = 0
+  var message = ""
   def sendMsg(txt: String) = Action {
-  	val filePath = "textFile.txt";
+  	val filePath = "txt/textFile" + fileCounter.toString + ".txt"
+  	fileCounter = fileCounter + 1
+  	message = fileCounter.toString + "\r\n" + txt
+    val writer = new PrintWriter(new File(filePath))
+    writer.write(message)
+    writer.close()
 
-    val writer = new PrintWriter(new File(filePath));
-    writer.write(txt);
-    writer.close();
+    Ok("This message is sent:\r\n" + message)
+  }
 
-    Source.fromFile(filePath).foreach {
-      print
-    }
+  def doSmth = Action {
+  	Ok("Smth is done.")
+  }
 
-    Ok("Your message is sent.")
+  def viewMsgs = Action {
+  	var allMessages = "\r\n"
+  	var i = 0
+  	for (i <- 0 to fileCounter - 1) {
+  	  val filePath = "txt/textFile" + i.toString + ".txt"
+  	  
+	  for (line <- Source.fromFile(filePath).getLines) {
+        allMessages = allMessages + line + "\r\n"
+	  }
+	}
+
+	Ok("All messages:" + allMessages)
   }
 
 }
